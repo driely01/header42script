a6afdd89549aa877a7116d80018f8f27eb5b47f9
driely01 <elyaagoubi.works@gmail.com>
2023-12-04 22:38:28 +0100

    made a 42 header checker

--- header.cpp
+++ header.cpp
@@ -6,7 +6,7 @@
 /*   By: del-yaag <del-yaag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 20:24:27 by del-yaag          #+#    #+#             */
-/*   Updated: 2023/12/04 22:20:40 by del-yaag         ###   ########.fr       */
+/*   Updated: 2023/12/04 22:34:27 by del-yaag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@
 #define RED     "\033[31m"
 #define GREEN   "\033[32m"
 
-int main( int argc, char **argv ) {
+int main( void ) {
 
 	DIR *dr;
 	struct dirent *en;
@@ -38,56 +38,55 @@ int main( int argc, char **argv ) {
 	std::ifstream file;
 	std::string login;
 
-	if ( argc == 2 ) {
+	std::cout << "enter login: ";
+	getline( std::cin, login ).eof();
+	if ( !login.length() )
+		return 0;
 		
-		dr = opendir(".");
-		if ( dr ) {
+	dr = opendir(".");
+	if ( dr ) {
 
-			while ( ( en = readdir( dr ) ) != NULL ) {
+		while ( ( en = readdir( dr ) ) != NULL ) {
 
-				std::string name = en->d_name;
-				findcpp = name.find( ".cpp" );
-				if ( findcpp != std::string::npos ) {
+			std::string name = en->d_name;
+			findcpp = name.find( ".cpp" );
+			if ( findcpp != std::string::npos ) {
+				
+				bool cheat = 1;
+				file.open( en->d_name );
+				if ( !file ) {
+
+					std::cout << "failed to open " << name << " file" << std::endl;
+					return 1;
+				}
+				std::string buffer;
+				while ( !getline( file, buffer ).eof() ) {
 					
-					bool cheat = 1;
-					file.open( en->d_name );
-					if ( !file ) {
+					findBy = buffer.find( "By: " );
+					findCreated = buffer.find( "Created: " );
+					findUpdated = buffer.find( "Updated: " );
+					
+					if ( findBy != std::string::npos ||
+							findCreated != std::string::npos ||
+							findUpdated != std::string::npos ) {
 
-						std::cout << "failed to open " << name << " file" << std::endl;
-						return 1;
-					}
-					std::string buffer;
-					while ( !getline( file, buffer ).eof() ) {
-						
-						findBy = buffer.find( "By: " );
-						findCreated = buffer.find( "Created: " );
-						findUpdated = buffer.find( "Updated: " );
-						
-						if ( findBy != std::string::npos ||
-							 findCreated != std::string::npos ||
-							 findUpdated != std::string::npos ) {
+						findlogin = buffer.find( login );
+						if ( findlogin == std::string::npos ) {
 
-							findlogin = buffer.find( argv[1] );
-							if ( findlogin == std::string::npos ) {
+							cheat = 0;
+							// std::cout << "	" << buffer << std::endl;
+							std::cout << RED << "cheater on file " << name << RESET << std::endl;
+						} else {
 
-								cheat = 0;
-								// std::cout << "	" << buffer << std::endl;
-								std::cout << RED << "cheater on file " << name << RESET << std::endl;
-							} else {
-
-								std::cout << GREEN << "ok " << name << RESET << std::endl;
-							}
+							std::cout << GREEN << "ok " << name << RESET << std::endl;
 						}
 					}
-					if ( cheat ) std::cout << GREEN << name << " is OK!" << RESET << std::endl;
 				}
-				file.close();
+				if ( cheat ) std::cout << GREEN << name << " is OK!" << RESET << std::endl;
 			}
-			closedir( dr );
+			file.close();
 		}
-	} else {
-
-		std::cout << "please you need to enter a login name" << std::endl;
+		closedir( dr );
 	}
 	return 0;
 }
