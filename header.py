92fa6596e7a745393adf2b7d151c392c3e23239e
driely01 <elyaagoubi.works@gmail.com>
2023-12-11 12:42:27 +0100

    add ascii art

--- header.py
+++ header.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import time
 
+# -----------------classes----------------- #
 class colors:
 	reset = '\033[0m'
 	bold = '\033[01m'
@@ -37,17 +38,33 @@ class colors:
 		purple = '\033[45m'
 		cyan = '\033[46m'
 		lightgrey = '\033[47m'
+# ----------------------------------------- #
 
-currentDirectory = os.getcwd()
+# ----------------------------------------- #
+print( f"\n\n{ colors.fg.cyan }\t /$$                              /$$                   /$$$$$$ /$$                        /$$      { colors.reset }" )     
+print( f"{ colors.fg.cyan }\t| $$                             | $$                  /$$__  $| $$                       | $$      { colors.reset }" )
+print( f"{ colors.fg.cyan }\t| $$$$$$$  /$$$$$$  /$$$$$$  /$$$$$$$ /$$$$$$  /$$$$$$| $$  \__| $$$$$$$  /$$$$$$  /$$$$$$| $$   /$${ colors.reset }" )
+print( f"{ colors.fg.cyan }\t| $$__  $$/$$__  $$|____  $$/$$__  $$/$$__  $$/$$__  $| $$     | $$__  $$/$$__  $$/$$_____| $$  /$$/{ colors.reset }" )
+print( f"{ colors.fg.cyan }\t| $$  \ $| $$$$$$$$ /$$$$$$| $$  | $| $$$$$$$| $$  \__| $$     | $$  \ $| $$$$$$$| $$     | $$$$$$/ { colors.reset }" )
+print( f"{ colors.fg.cyan }\t| $$  | $| $$_____//$$__  $| $$  | $| $$_____| $$     | $$    $| $$  | $| $$_____| $$     | $$_  $$ { colors.reset }" )
+print( f"{ colors.fg.cyan }\t| $$  | $|  $$$$$$|  $$$$$$|  $$$$$$|  $$$$$$| $$     |  $$$$$$| $$  | $|  $$$$$$|  $$$$$$| $$ \  $${ colors.reset }" )
+print( f"{ colors.fg.cyan }\t|__/  |__/\_______/\_______/\_______/\_______|__/      \______/|__/  |__/\_______/\_______|__/  \__/{ colors.reset }" )
+print( f"\n                                                     By: { colors.bold + colors.fg.yellow } DEL-YAAG { colors.reset }[driely]" )
+# ----------------------------------------- #
 
-inputLogin = input( f"{ colors.bold + colors.bg.orange + colors.fg.black }Enter login:{ colors.reset } " )
-if not len( inputLogin ) or inputLogin.isspace():
-	exit( 0 )
+# ----------------functions---------------- #
+# loaing func
+def loading( number ):
+	for i in range( number ):
+		print(f"{ colors.bg.purple + colors.fg.black }#{ colors.reset }", end="", flush=True)
+		time.sleep(0.05)
+	print("")
 
-# check norminette
-print( f'\n{ colors.bg.green + colors.fg.black }  -- Check Norminette --  { colors.reset }' )
-time.sleep( 1 )
-result = subprocess.run( [ "norminette" ], shell=True )
+# split the path and return new one from the working directory
+def filePathFromWD( filePath, directory ):
+	find = filePath.find( directory )
+	newPath = filePath[ find + 1 + len(directory): ].strip()
+	return newPath
 
 # check login in file
 def openFileAndCheckLogin( filePath, newPath, fileName ):
@@ -72,12 +89,6 @@ def openFileAndCheckLogin( filePath, newPath, fileName ):
 		if not isHeader:
 			print( f"{ colors.fg.yellow + newPath }missing header in { fileName }! { colors.reset }" )
 
-# split the path and return new one from the working directory
-def filePathFromWD( filePath, directory ):
-	find = filePath.find( directory )
-	newPath = filePath[ find + 1 + len(directory): ].strip()
-	return newPath
-
 # walk in the workin directory and its subdirectories and open all files and check them
 def checkFilesForAutor( directory ):
 	totalFiles = 0
@@ -94,6 +105,19 @@ def checkFilesForAutor( directory ):
 			percentage = ( ( occurences / totalFiles ) * 100 ) / 3
 			formattedPercentage = "{:.2f}".format( percentage )
 			print( f'{ colors.fg.yellow + formattedPercentage }%\t\t{ loginName + colors.reset }' )
+# ----------------------------------------- #
+
+# ------------------start------------------ #
+# get the intput
+inputLogin = input( f"{ colors.bold + colors.fg.orange }Enter login:{ colors.reset } " )
+if not len( inputLogin ) or inputLogin.isspace():
+	exit( 0 )
+
+
+# check norminette
+print( f'\n{ colors.bg.green + colors.fg.black }  -- Check Norminette --  { colors.reset }' )
+loading( 26 )
+result = subprocess.run( [ "norminette" ], shell=True )
 
 # split the logins and store them in dictionary
 login = inputLogin.split( ' ' )
@@ -101,8 +125,9 @@ login = [ part.strip() for part in login if part.strip() ]
 loginOccurences = { loginName: 0 for loginName in login }
 
 # check haders in all files
-print( f'\n{ colors.bg.green + colors.fg.black }  -- Check Headers --  { colors.reset }')
-time.sleep( 1 )
+print( f'\n{ colors.bg.green + colors.fg.black }    -- Check Header --    { colors.reset }')
+loading( 26 )
+currentDirectory = os.getcwd()
 checkFilesForAutor( currentDirectory )
 print( f'______________________________' )
 
@@ -112,3 +137,4 @@ if result.returncode == 0:
 else:
 	print( f'{ colors.fg.red }Norminette\tKO{ colors.reset }' )
 print( f'______________________________' )
+# ----------------------------------------- #
